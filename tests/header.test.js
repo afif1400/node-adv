848362bc50878a78cf30ad3ee4b6a95003139430
afif1400 <afifahmed456123@gmail.com>
2021-10-29 18:51:43 +0530

    added test suites for auth and blogs

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,20 +1,19 @@
-const puppeteer = require('puppeteer');
-let browser, page;
+const Page = require('./helpers/page')
+
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  })
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000')
 })
 
 afterEach(async () => {
-  await browser.close()
+  await page.close()
 })
 
 test('the header has the correct test', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo')
 
   expect(text).toEqual('Blogster')
 })
@@ -27,30 +26,11 @@ test('clicking login starts oauth flow', async () => {
   expect(url).toMatch(/accounts\.google\.com/)
 })
 
-test.only('When signed in, shows logout button', async () => {
-  const id = '6175180f65af4459e02ed2a5';
+test('When signed in, shows logout button', async () => {
 
-  const Buffer = require('safe-buffer').Buffer;
-
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  }
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
-
-  const Keygrip = require("keygrip");
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey])
-  const sig = keygrip.sign('session=' + sessionString)
-
-  await page.setCookie({ name: 'session', value: sessionString })
-  await page.setCookie({ name: 'session.sig', value: sig })
-  await page.goto('localhost:3000');
-  await page.waitFor('a[href="/auth/logout"]');
-
-  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+  await page.login()
+  const text = await page.getContentsOf('a[href="/auth/logout"]')
 
   expect(text).toEqual('Logout')
 })
+
